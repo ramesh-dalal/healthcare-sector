2594a10bcd7171f2bf4bfffdf74f7ac0b7689316
ramesh-dalal <151204265+ramesh-dalal@users.noreply.github.com>
2024-02-16 20:31:26 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt install openjdk-11-jdk -y
+sudo apt-get install docker.io -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" 
+sudo swapoff -a
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
